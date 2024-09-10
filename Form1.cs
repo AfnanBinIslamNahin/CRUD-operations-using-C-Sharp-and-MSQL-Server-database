a8e2ba6a6394e5ca13dfe489bc6eb7b5d5340468
AfnanNahin <149258952+AfnanBinIslamNahin@users.noreply.github.com>
2024-09-10 21:12:40 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureDataGridView();
+            textBox1.Text = "Employee ID";
+            textBox1.ForeColor = Color.Silver;
+            
+        }
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\afnan\\OneDrive\\Documents\\CURDFORM.mdf;Integrated Security=True;");
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("insert into CURDForm values (@id,@name,@salary,@city)", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@salary", textBox3.Text);
+            cmd.Parameters.AddWithValue("@city", textBox4.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+
+            MessageBox.Show("Successfully save");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\afnan\\OneDrive\\Documents\\CURDFORM.mdf;Integrated Security=True;");
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update CURDForm set name=@name,salary=@salary,city=@city where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@salary", textBox3.Text);
+            cmd.Parameters.AddWithValue("@city", textBox4.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully update");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\afnan\\OneDrive\\Documents\\CURDFORM.mdf;Integrated Security=True;");
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete CURDForm where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully delete");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\afnan\\OneDrive\\Documents\\CURDFORM.mdf;Integrated Security=True;");
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from CURDForm where id = @id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void ConfigureDataGridView()
+        {
+            // Set alternating row colors for readability
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+
+            // Set header styles
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(255, 128, 0);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+
+            // Set grid line color
+            dataGridView1.GridColor = Color.Black;
+
+            // Set default row styles
+            dataGridView1.DefaultCellStyle.BackColor = Color.White;
+            dataGridView1.DefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+
+            // Set selection styles
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.DarkOrange;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.White;
+
+            // Fit columns to the DataGridView
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Adjust row height to fit content
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            // Disable column resize by the user for a consistent layout
+            dataGridView1.AllowUserToResizeColumns = false;
+
+            // Set row and column headers visibility if needed
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = true;
+        }
+       
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[CURDForm]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Age] VARCHAR(50) NULL
+)
